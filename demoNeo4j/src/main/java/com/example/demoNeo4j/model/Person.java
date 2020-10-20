b2df6224f4437f99e9e26057b9598a129314cc8d
sago0519 <santosh.goudar@netcracker.com>
2020-10-20 15:45:41 +0530

    Demo

--- demoNeo4j/src/main/java/com/example/demoNeo4j/model/Person.java
+++ demoNeo4j/src/main/java/com/example/demoNeo4j/model/Person.java
@@ -2,8 +2,6 @@ package com.example.demoNeo4j.model;
 
 
 import java.util.List;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
